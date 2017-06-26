b1274d395a411f191345c3c457c82cd3fa2fe61e
raul <raul@raul-QOSMIO-F755>
2017-06-26 01:54:09 -0500

    refactoring shava-security to shava-common

---
+++ architecture-base/shava-entitymanager/src/main/java/com/shava/entitymanager/entity/BaseEntity.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shava.entitymanager.entity;
+
+import java.time.LocalDateTime;
+
+
+public class BaseEntity {
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private String createdBy;
+    private String updatedBy;
+
+    /**
+     * @return the createdDate
+     */
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    /**
+     * @param createdDate the createdDate to set
+     */
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    /**
+     * @return the updatedDate
+     */
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    /**
+     * @param updatedDate the updatedDate to set
+     */
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    /**
+     * @return the createdBy
+     */
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    /**
+     * @param createdBy the createdBy to set
+     */
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    /**
+     * @return the updatedBy
+     */
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    /**
+     * @param updatedBy the updatedBy to set
+     */
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+    
+}
